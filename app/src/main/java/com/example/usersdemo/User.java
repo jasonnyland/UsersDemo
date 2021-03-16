59586ec65c6f077f3944cec61335813d7e166582
Jason Nyland <jason@jasonnyland.com>
2021-03-16 06:59:52 -0700

    users api populates recyclerview

--- app/src/main/java/com/example/usersdemo/User.java
+++ app/src/main/java/com/example/usersdemo/User.java
@@ -3,10 +3,15 @@ package com.example.usersdemo;
 import java.util.ArrayList;
 
 public class User {
+    private Integer id;
     private String name;
 
-    public User(String name) {
-        this.name = name;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -15,15 +20,5 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public static ArrayList<User> createUserList(int count) {
-        ArrayList<User> users = new ArrayList<User>();
-
-        for (int i = 1; i <= count; i++) {
-            users.add(new User("User Number " + i));
-        }
-
-        return users;
     }
 }
--- app/src/main/java/com/example/usersdemo/UserListActivity.java
+++ app/src/main/java/com/example/usersdemo/UserListActivity.java
@@ -5,23 +5,69 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class UserListActivity extends AppCompatActivity {
 
     private RecyclerView userRecycler;
-    ArrayList<User> users;
+    List<User> users;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
+        users = new ArrayList<>();
         userRecycler = findViewById(R.id.userRecycler);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        userRecycler.setLayoutManager(linearLayoutManager);
+        volleyRequest();
 
-        users = User.createUserList(20);
-        UserListAdapter adapter = new UserListAdapter(users);
-        userRecycler.setAdapter(adapter);
-        userRecycler.setLayoutManager(new LinearLayoutManager(this));
+    }
 
+
+    public void volleyRequest() {
+        users = new ArrayList<>();
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,
+                "https://jsonplaceholder.typicode.com/users", null, new com.android.volley.Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray dataArray) {
+                try {
+                    for (int i = 0; i < dataArray.length(); i++) {
+                        JSONObject userData = dataArray.getJSONObject(i);
+                        User user = new User();
+                        user.setId(userData.getInt("id"));
+                        user.setName(userData.getString("name"));
+                        users.add(user);
+                    }
+                    UserListAdapter adapter = new UserListAdapter(users);
+                    userRecycler.setAdapter(adapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(UserListActivity.this, "Error: "+error.networkResponse, Toast.LENGTH_SHORT).show();
+                Log.d("ULA", error.toString());
+            }
+        });
+
+        MySingleton.getInstance(this).addToRequestQueue(jsonArrayRequest);
     }
 }
