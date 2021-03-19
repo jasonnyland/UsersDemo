2c50e9e1d54d917263b2dace27a9ce982b943617
Jason Nyland <jason@jasonnyland.com>
2021-03-19 11:03:07 -0700

    added notification to details page

--- app/src/main/java/com/example/usersdemo/UserDetailsActivity.java
+++ app/src/main/java/com/example/usersdemo/UserDetailsActivity.java
@@ -1,9 +1,15 @@
 package com.example.usersdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +25,7 @@ public class UserDetailsActivity extends AppCompatActivity {
     TextView detailsEmail;
     ImageView avatarView;
     Button saveButton;
+    boolean passing = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,5 +66,47 @@ public class UserDetailsActivity extends AppCompatActivity {
                         Toast.LENGTH_SHORT).show();
             }
         });
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("Details Notification", "Details Notification",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        passing = false;
+        super.onResume();
+    }
+
+    @Override
+    public void onBackPressed() {
+        passing = true;
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (!passing) sendNotification();
+    }
+
+    public void sendNotification() {
+        String msg = "Please Come Back!";
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(UserDetailsActivity.this, "Details Notification");
+        builder.setContentTitle("UsersDemo");
+        builder.setContentText(msg);
+        builder.setSmallIcon(android.R.drawable.stat_notify_error);
+        builder.setAutoCancel(true);
+        Intent intent = new Intent(UserDetailsActivity.this,
+                UserDetailsActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(UserDetailsActivity.this,
+                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingIntent);
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(UserDetailsActivity.this);
+        managerCompat.notify(1, builder.build());
     }
 }
